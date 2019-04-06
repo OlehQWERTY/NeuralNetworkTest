50122cc91ed517ea506ce6973ec105ed7b910688
Unknown <oleggordiichenko@gmail.com>
2019-04-06 18:29:18 +0300

    linux adaptation and a b c d e (16)

--- autolaunch.py
+++ autolaunch.py
@@ -10,13 +10,13 @@ else:
 	# Python 2 code in this block
 
 
-
+import os
 import subprocess
 import multiprocessing
 import time
-coresAmmount = multiprocessing.cpu_count()
+coresAmmount = int(multiprocessing.cpu_count()/2 - 2) # -2 in order to have normal performanse for avarage usage
 # coresAmmount = 2
-# print(coresAmmount, "cores is detected")
+print(coresAmmount, "threads is started")
 
 dictProc = {}
 for i in range(coresAmmount):
@@ -24,8 +24,11 @@ for i in range(coresAmmount):
 
 for infin in range(4):  # ~ 8 (32) hours of work
 	for i in range(coresAmmount):
-		print(i)
-		dictProc[i] = subprocess.Popen(['python', 'main.py'])  # python3 - Ubuntu
+		# python - windows and ubuntu - python3 command
+		if os.name == 'posix':
+			dictProc[i] = subprocess.Popen(['python3', 'main.py'])  # python3 - Ubuntu
+		else:
+			dictProc[i] = subprocess.Popen(['python', 'main.py'])  # python - Windows
 
 	time.sleep(4000)  # ~ 16 (1000) min of work
 
